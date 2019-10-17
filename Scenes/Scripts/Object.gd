3d623f326649c95e28db5af3431c2b27ae75d8f9
Kidoncio <lucas.kidoncio@gmail.com>
2019-10-17 19:42:59 -0300

    Fix para EASE_TYPE

--- Scenes/Scripts/Object.gd
+++ Scenes/Scripts/Object.gd
@@ -4,7 +4,7 @@ extends Node2D
 onready var TweenNode: Tween = get_node("Tween")
 
 export (int, 0, 10, 1) var TRANSITION_TYPE: int = 10
-export (int, 0, 3, 1) var EASY_TYPE: int = 1
+export (int, 0, 3, 1) var EASE_TYPE: int = 1
 
 const NODE_PROPERTY: String = "position"
 
@@ -19,6 +19,6 @@ func go_to_mouse_pos():
 	var mouse_pos = get_global_mouse_position()
 	var current_pos = self.global_position
 	
-	TweenNode.interpolate_property(self, NODE_PROPERTY, current_pos, mouse_pos, 1.0, TRANSITION_TYPE, EASY_TYPE)
+	TweenNode.interpolate_property(self, NODE_PROPERTY, current_pos, mouse_pos, 1.0, TRANSITION_TYPE, EASE_TYPE)
 	TweenNode.start()
 	pass
